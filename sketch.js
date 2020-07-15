84a3af6cd79cb4b24204e9d335c70e7ea4309fd4
Ewoud Dronkert <ewoud@dronkert.net>
2020-07-15 18:54:50 +0200

    Simpler

--- sketch.js
+++ sketch.js
@@ -1,90 +1,66 @@
-const N = 10000;
-const branch = 15;
-const angle = 0.15;
-const alph = 20;
-const perframe = 25;
+// The Collatz Conjecture
+// As seen on The Coding Train by Daniel Shiffman
+// https://youtu.be/EYLWxwo1Ed8
+// https://thecodingtrain.com/CodingInTheCabana/002-collatz-conjecture.html
 
-const tree = new Array(N + 1);  // element 0 not used
-let countdown = N;
+const N = 10000;
+const len = 11;
+const thick = 3;
+const branch = len - thick + 1;
+const angle = 0.15;
+const soft = 10;
+const batch = 50;
+const fg = 255;
+const bg = 51;
+let n = 2;  // n=1 is the root of the tree, first branch is from 1 to 2
 
 function setup() {
-  createCanvas(1200, 1100);
-  background(51);
-  stroke(255, alph);
-  strokeWeight(3);
-  noFill();
-
-  // Initialise array
-  for (let i = 0; i <= N; ++i)
-    tree[i] = 0;
-
-  // Populate tree according to Collatz
-  // Result: every n <= N reduces to tree[n], e.g.:
-  //   tree[5] = 8 because (5*3+1)/2 == 8
-  //   tree[8] = 4 because 8/2 == 4
-  for (let i = 1; i <= N; ++i) {
-    let up = i * 2;
-    if (up <= N)
-      tree[up] = i;
-    if (--up % 3 == 0) {
-      up /= 3;
-      if (up > 1)
-        tree[up] = i;
-    }
-  }
+	createCanvas(1500, 1000);
+	background(bg);
+	stroke(fg, soft);
+	strokeWeight(thick);
 }
 
 function draw() {
-  // Origin near the bottom, slightly right of the middle
-  translate(width / 30 * 16, height - 50);
-  scale(1, -1);
 
-  // Draw multiple branches every frame
-  for (let dummy = 0; dummy < perframe; ++dummy) {
-    // Check every tree index from N downto 2
-    if (countdown > 1) {
-      let leaf = countdown--;
+	// Origin near the bottom
+	translate(width / 2, height - 25);
+	scale(1, -1);
 
-      // Some indices don't reduce inside the range 1..N
-      if (tree[leaf]) {
+	// Draw multiple branches every frame
+	for (let dummy = 0; dummy < batch; ++dummy) {
 
-        // Follow the path all the way down and reverse it
-        const path = [];
-        while (leaf) {
-          path.push(leaf);
-          leaf = tree[leaf];
-        }
-        path.reverse();
+		if (n <= N) {
 
-        // Draw the path from the ground up
-        push();
-        // Element 0 is always 1, so start at element 1
-        for (let i = 1; i < path.length; ++i) {
+			// Calculate sequence of n downto 1
+			const seq = [];
+			let i = n++;
+			while (i != 1) {
+				seq.push(i);
+				i = i % 2 ? (i * 3 + 1) / 2 : i / 2;  // (i*3+1)/2 for fewer even numbers
+			}
 
-          // Veer left or right
-          rotate(path[i] % 2 ? -angle : angle);
-          
-          // Draw branch straight up (with current rotation)
-          // Minus 2 to minimise overlap
-          line(0, 0, 0, branch - 2);
-          
-          // Climb up the branch
-          translate(0, branch);
-        }
-        pop();
-      }
-    }
-  }
+			// Draw the complete branch bottom up
+			seq.reverse();
+			push();
+			for (const i of seq) {
+				rotate(i % 2 ? angle : -angle);
+				line(0, 0, 0, branch);
+				translate(0, len);
+			}
+			pop();
+		}
+	}
 
-  // Progress bar
-  resetMatrix();
-  line(3, 3, map(countdown, N, 1, 0, width), 3);
-
-  if (countdown < 2) {
-    // Erase progress bar and stop
-    stroke(51);
-    strokeWeight(6);
-    line(0, 3, width, 3);
-    noLoop();
-  }
+	// Progress bar
+	resetMatrix();
+	if (n <= N) {
+		line(thick, thick, map(n, 0, N, 0, width), thick);
+	} else {
+		// Erase and stop
+		stroke(bg);
+		strokeWeight(thick * 2);
+		line(0, thick, width, thick);
+		noLoop();
+	}
 }
